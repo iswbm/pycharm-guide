2c76c35780c379c40273bc6fa91f4061dfe44652
iswbm <wongbingming@163.com>
2021-07-09 01:44:11 +0800

    支持多版本构建

--- source/conf.py
+++ source/conf.py
@@ -28,7 +28,7 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['chinese_search','sphinx.ext.mathjax', 'sphinx_multiversion']
+extensions = ['chinese_search','sphinx.ext.mathjax', 'sphinx_sitemap','sphinx_multiversion']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -129,7 +129,7 @@ release = '2.0'
 version = '2.0'
 
 # Options for extensions.
-
+html_baseurl = 'http://pycharm.iswbm.com'
 html_sidebars = {
     '**': [
         'versioning.html',
@@ -137,3 +137,4 @@ html_sidebars = {
 }
 
 smv_latest_version = 'win'
+sitemap_url_scheme = "{link}"
